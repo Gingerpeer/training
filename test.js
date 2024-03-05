a619fc253b877dec95703a63d8fb44a2885797f8
Pierre van Zyl <74056285+Gingerpeer@users.noreply.github.com>
2024-03-05 11:23:15 +0200

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-function showMessage(hideOrShow) {
-  if(hideOrShow){
-    document.getElementById("demo").innerHTML = "Hello, JavaScript!";
-  }else{
-    document.getElementById("demo").innerHTML = "";
-  }
-}
